aeb9da02f3609b9ed8281fbaf367fc6c818a2727
Atik Shakur <atik.shakur98@gmail.com>
2022-04-23 20:11:25 +0600

    updated

--- app/src/main/java/com/example/coroutine/retrofit/ApiInterface.kt
+++ app/src/main/java/com/example/coroutine/retrofit/ApiInterface.kt
@@ -1,10 +1,11 @@
 package com.example.coroutine.retrofit
 
 import com.example.coroutine.model.Post
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiInterface {
 
     @GET("posts")
-    suspend fun getAllPost() : List<Post>
+    suspend fun getAllPost() : Response<List<Post>>
 }
